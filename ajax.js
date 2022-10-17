d2ed659513cde9cc43ba0975b8a0d689fbee01b0
sakamoto-dai <sakamoto555dai@gmail.com>
2022-10-18 07:43:48 +0900

    ページ遷移

--- ajax.js
+++ ajax.js
@@ -1,30 +1,54 @@
 $(document).ready(function(){
+
     $("#login_btn").click(function(){
         let wUserId = $("#userid").val();
         let wPass = $("#pass").val();
-        console.log(wUserId,wPass);
+        console.log(wUserId,wPass,);
 
         $.ajax({
             url : "check.php",
             type : "post",
-            //datatype : "",
+            datatype : "json",
             data : {
                 "userid" : wUserId,
                 "pass" : wPass
             },
         })
         .done(function(data){
-            console.log('[responce data:'+data+']');
-            switch(data){
+            console.log(data);
+            //json文字列をjavascriptオブジェクトに変換
+            var data_json = JSON.parse(data);
+            //オブジェクトの中身確認。中身へのアクセス方法は以下どちらでも可
+            //オブジェクト.プロパティ名
+            //オブジェクト[プロパティ名]
+            console.log('[responce result: '+data_json.result + ']');
+            console.log('[responce msg: '+data_json["success_msg"]+ ']');
+            switch(data_json["result"]){
                 case 'OK':
-                    $("#result").text("ログイン成功");
+                    location.href = "mypage.html";
+                    //$("#result").text("ログイン成功");
                     break;
                 case 'pass NG':
                     $("#result").text("パスワードが違います");
                     break;
                 case 'userid NG':
-                    $("#result").text("ユーザIDはありません");
+                    $("#result").text("このユーザIDは登録されていません");
                     break;
+                case 'block user NG':
+                    $("#result").text("残念ながらこのユーザはご利用を制限させていただいております");
+                    break;
+                default:
+                    $("#result").text("例外");
+            }
+            switch(data_json["success_msg"]){
+                case 0:
+                    $("#success_msg").text("はじめまして！");
+                    break;
+                case 1:
+                    $("#success_msg").text("今日も頑張りましょう");
+                    break;
+                default : 
+                    $("#success_msg").text("");
 
             }
         });
--- check.php
+++ check.php
@@ -1,49 +1,167 @@
 <?php
+
+    define("IDX_ALL_PASS",0);
+    define("IDX_BLOCK",1);
+    define("FILE_NAME","config/check.cfg");
+    define("USER_INFO_FILE","userinfo.js");
+    define("_ON",1);
+    define("_OFF",1);
+    $cfg = array();
+    $resp = array();
+    $userInfoArray = array();
+
     if(isset($_POST["userid"], $_POST["pass"]))
     {
-       // print($_POST["userid"]);
+        global $resp;
+        //print($_POST["userid"]);
         //print($_POST["pass"]);
+
+        //設定値読み込み
+        input_config();
 
         $id = $_POST["userid"];
         $pass = $_POST["pass"];
+    
+        check($id, $pass);
+
+        //echo $ret;
+        echo json_encode($resp);
+
+    }
+
+    //id,pass渡して、認証処理してくれる。メッセージを返す
+    function check($id, $pass)
+    {
+        global $cfg;
+        global $resp;
+        global $userInfoArray;
+        //ALLPASSが有効なら、認証処理無
+        if($cfg[IDX_ALL_PASS] == _ON){
+            return 'OK';
+        }
+
         //userinfo.jsと照合
-        $url = "userinfo.js";
-        //jsonファイルからjsonデータ読み込み
+        $url = USER_INFO_FILE;
+        //jsonファイル文字列に変換
         $json = file_get_contents($url);
         
-        //jsonデータを連想配列に変換
-        $arr = json_decode($json,true);
-
-        $last = count($arr[userinfolist]);
-        $cnt = 0;
+        //json文字列を連想配列に変換
+        $userInfoArray = json_decode($json,true);
+        $last = count($userInfoArray[userinfolist]);
+        $cnt = 1;
         //print($last);
-        foreach($arr[userinfolist] as $key => $val){
-            $cnt++;
-            if($val[userid] == $id)
-            {
-                //echo '[matchkey:'.$key.']';
-                if($val[pass] == $pass) 
-                {
-                    echo 'OK';
-                    break;
-                }
-                else{
-                    echo 'pass NG';
-                    break;
-                }
-            }
-            //print($cnt);
-            if($cnt >= $last)
-            {
-                echo 'userid NG';
-            }
-
-            /*
+        foreach($userInfoArray[userinfolist] as $key => $val){
             //jsonの中身参照
+            /*
             echo 'key:'.$key.' ';
             echo 'userid:'.$val[userid].' ';
             echo 'pass:'.$val[pass].'       ';
             */
+            if($val[userid] == $id)
+            {
+                //echo '[matchkey:'.$key.']';
+                if($cfg[IDX_BLOCK] == _ON)
+                {
+                    if($val[block] == _ON)
+                    {
+                        $resp['result'] = 'block user NG';
+                        break;
+                    }
+                }
+
+                if($val[pass] == $pass) 
+                {
+                    $resp['result'] = 'OK';
+                    if($val['latest_login_date'] == "")
+                    {
+                        $resp['success_msg'] = 0;
+                    }
+                    else{
+                        $resp['success_msg'] = 1;
+                    }
+                    //日付け書きみ
+                    write_date($key);
+                    break;
+                }
+                else{
+                    $resp['result'] = 'pass NG';
+                    break;
+                }
+
+            }
+            //print($cnt);
+            if($cnt >= $last)
+            {
+                $resp['result'] = 'userid NG';
+                break;
+            }
+            $cnt++;
+
         }
+
+    }
+
+    //設定ファイル読み込んで、グローバル変数に入れる
+    function input_config()
+    {
+        global $cfg;
+
+        //設定ファイル読み込み
+        $fp = fopen(FILE_NAME, "r");
+        if(!$fp)
+        {
+            exit("ファイル読み込みで失敗");
+        }else{
+            //共有ロック
+            flock($fp, LOCK_UN[2]);
+            $i = 0;
+            while(!feof($fp))
+            {
+                //eofまで読み込み
+                $cfgStr = fgets($fp);
+                //空白削除
+                //echo $cfgStr;
+                //$cfgStr = trim($cfgStr," "); 
+                $cfgStr = str_replace(" ", "", $cfgStr);
+                //echo $cfgStr;
+                //=以降の文字を取得
+                $cfgStr = explode("=",$cfgStr);
+                //改行・リターン文字削除
+                $cfgStr[1] = str_replace("\n", "", $cfgStr[1]);
+                $cfgStr[1] = str_replace("\r", "", $cfgStr[1]);
+                //echo $cfgStr[1];
+
+                //設定値をグローバル変数に格納
+                $cfg[$i] = $cfgStr[1]; 
+                $i++;
+            }
+            //echo "[allpass".$cfg[IDX_ALL_PASS]."]";
+            //echo "[block".$cfg[IDX_BLOCK]."]";
+            //ロック解除
+            flock($fp, LOCK_UN[3]);
+            fclose($fp);
+        }
+
+    }
+
+    function write_date($key)
+    {
+        global $userInfoArray;
+        $date = date("Y/m/d-H:i:s");
+        $userInfoArray[userinfolist][$key][latest_login_date] = $date;
+        $json = json_encode($userInfoArray);
+
+        
+        // 読みこみだと成功
+        $fp = fopen("./userinfo.js","w+");
+        if(!$fp)
+        {
+            exit("ファイルopen失敗");
+        }
+        else{
+            fputs($fp, $json);
+        }
+
+
     }
 ?>
